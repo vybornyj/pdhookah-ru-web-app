aff338200d2f083c218c9070900c214307149a1b
Denis Vybornyj <denis.vybornyj@gmail.com>
2020-07-29 20:02:49 +0300

    test deploy from no master

---
+++ pages/test.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const Page = () => (
+  <div className='container'>
+    <Head>
+      <title>TEST</title>
+    </Head>
+
+    <main>
+      <h1 className='title'>TEST</h1>
+    </main>
+  </div>
+)
+
+export default Page
